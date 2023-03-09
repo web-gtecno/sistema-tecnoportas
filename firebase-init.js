f954de15b06d3ee67fdaecc4974784c7438ab718
Anderson Domingos <andomingos87@gmail.com>
2023-03-09 14:38:38 -0300

    Coloquei o JS do Firebase separado

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB18dW4WK48y5VFbaDJrxi-V5-QSV8A3sY",
+    authDomain: "tecnoportas-rewards.firebaseapp.com",
+    projectId: "tecnoportas-rewards",
+    storageBucket: "tecnoportas-rewards.appspot.com",
+    messagingSenderId: "501031301545",
+    appId: "1:501031301545:web:21b715a0170a6d9eec14b8"
+};
+firebase.initializeApp(firebaseConfig);
